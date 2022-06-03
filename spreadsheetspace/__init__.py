d0759cfe17e3123aed8f53f9e1a9ff6b1805e62d
Ahmad Alobaid <ahmad88me@gmail.com>
2022-06-03 14:41:18 +0200

    add table_to_content

--- spreadsheetspace/__init__.py
+++ spreadsheetspace/__init__.py
@@ -5,9 +5,10 @@ def table_to_content(table_url):
     df = pd.read_csv(table_url)
     content = []
     header = df.columns
-    for row in df.rows:
+    for idx, row in df.iterrows():
         r = []
         for h in header:
             r.append(row[h])
         content.append(r)
     return content
+
